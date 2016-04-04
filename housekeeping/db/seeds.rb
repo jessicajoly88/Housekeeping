e17cca66fe0c45242f68e3a8f9bbb3a2cfb48aa8
Jessica <jessicajoly88@gmail.com>
2016-04-04 14:40:28 -0400

    Add individual user show page.

--- housekeeping/db/seeds.rb
+++ housekeeping/db/seeds.rb
@@ -1,2 +1,5 @@
+Users = User.create([{ name:'Jane Smith', schedule: 'March 24 2016, 2pm' }])
 
-  Users = User.create([{ name:'Jane Smith', schedule: 'Monday, March 24 2016' }])
+Apartments = Apartment.create([{ name:'Plateau Loft', address: '1525 des Pins, Montreal, V5L4H1', duration_estimate: "2 hours" }])
+
+Cleanings = Cleaning.create([{ user_id: 1, apartment_id: 1 }])
